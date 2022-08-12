6f32340e0c2a1bb028707720271e2b06c098a520
Thiago Martins <thiago_valentim1@hotmail.com>
2022-08-12 17:18:10 -0300

    feat: ensure HelloCdkStack has bucket resource with versioning enabled

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,6 +1,7 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import { aws_s3 as s3 } from "aws-cdk-lib";
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,8 +10,12 @@ export class HelloCdkStack extends cdk.Stack {
     // The code that defines your stack goes here
 
     // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const queue = new sqs.Queue(this, "HelloCdkQueue", {
+      visibilityTimeout: cdk.Duration.seconds(300),
+    });
+
+    new s3.Bucket(this, "MyFirstBucket", {
+      versioned: true,
+    });
   }
 }
--- test/hello-cdk.test.ts
+++ test/hello-cdk.test.ts
@@ -1,17 +1,31 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as HelloCdk from '../lib/hello-cdk-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import * as HelloCdk from "../lib/hello-cdk-stack";
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/hello-cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new HelloCdk.HelloCdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test("SQS Queue Created", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new HelloCdk.HelloCdkStack(app, "MyTestStack");
+  // THEN
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties("AWS::SQS::Queue", {
+    VisibilityTimeout: 300,
+  });
+});
+
+test("S3 Bucket Created", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new HelloCdk.HelloCdkStack(app, "MyTestStack");
+  // THEN
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties("AWS::S3::Bucket", {
+    VersioningConfiguration: {
+      Status: "Enabled",
+    },
+  });
 });
