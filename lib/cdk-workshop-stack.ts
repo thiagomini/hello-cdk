9662e0bda137a9f41e264899c01e45fe07266ce7
Thiago Martins <thiago_valentim1@hotmail.com>
2022-08-15 17:26:08 -0300

    refactor(lib): extract CdkWorkshopStack logic to private methods

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -4,28 +4,42 @@ import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
 import { HitCounter } from "./hitcounter.construct";
 import { TableViewer } from "cdk-dynamo-table-viewer";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const helloLambda = new lambda.Function(this, "HelloHandler", {
+    const helloLambda = this.buildHelloLambda();
+    const helloWithCounter = this.wrapLambdaInHitCounter(helloLambda);
+    this.buildApiGatewayFor(helloWithCounter);
+    this.attachTableViewerTo(helloWithCounter.hitCounterTable);
+  }
+
+  private buildHelloLambda(): lambda.Function {
+    return new lambda.Function(this, "HelloHandler", {
       runtime: lambda.Runtime.NODEJS_16_X,
       code: lambda.Code.fromAsset("lambda"), // Code loaded from "lambda" directory
       handler: "hello.handler", // File is "hello" and function is "handler"
     });
+  }
 
-    const helloWithCounter = new HitCounter(this, "HelloHitCounter", {
-      downstream: helloLambda,
+  private wrapLambdaInHitCounter(lambda: lambda.Function): HitCounter {
+    return new HitCounter(this, "HelloHitCounter", {
+      downstream: lambda,
     });
+  }
 
+  private buildApiGatewayFor(hitCounter: HitCounter): void {
     new apigw.LambdaRestApi(this, "Endpoint", {
-      handler: helloWithCounter.counterFunctionHandler,
+      handler: hitCounter.counterFunctionHandler,
     });
+  }
 
+  private attachTableViewerTo(dynamodbTable: dynamodb.Table): void {
     new TableViewer(this, "ViewHitCounter", {
       title: "Hello Hits",
-      table: helloWithCounter.hitCounterTable,
+      table: dynamodbTable,
     });
   }
 }
